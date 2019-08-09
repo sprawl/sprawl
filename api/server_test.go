15b27ac8623cf8a7f1a05191a2b7942800e53355
Jani Anttonen <jantto@utu.fi>
2019-08-09 11:50:52 +0300

    update tests

--- api/server_test.go
+++ api/server_test.go
@@ -16,8 +16,12 @@ const bufSize = 1024 * 1024
 
 var lis *bufconn.Listener
 
+var client OrderHandlerClient
+var ctx context.Context
+
 // Construct a correct test Order
 var testOrder = CreateRequest{Asset: []byte("ETH"), CounterAsset: []byte("BTC"), Amount: 52617562718, Price: 0.1}
+var lastOrder *Order
 
 func init() {
 	lis = bufconn.Listen(bufSize)
@@ -28,22 +32,28 @@ func init() {
 			log.Fatalf("Server exited with error: %v", err)
 		}
 	}()
+	ctx = context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithDialer(bufDialer), grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	client = NewOrderHandlerClient(conn)
 }
 
 func bufDialer(string, time.Duration) (net.Conn, error) {
 	return lis.Dial()
 }
 
-func TestGrpc(t *testing.T) {
-	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithDialer(bufDialer), grpc.WithInsecure())
-	assert.Nil(t, err)
-
-	defer conn.Close()
-	client := NewOrderHandlerClient(conn)
+func TestOrderCreation(t *testing.T) {
 	resp, err := client.Create(ctx, &testOrder)
+	assert.Equal(t, nil, err)
+	t.Log("Created Order: ", resp)
+	assert.NotEqual(t, false, resp)
+	lastOrder = resp.GetCreatedOrder()
+}
 
-	assert.Nil(t, err)
-	t.Log(resp)
+func TestOrderDeletion(t *testing.T) {
+	resp, err := client.Delete(ctx, &OrderSpecificRequest{Id: lastOrder.GetId()})
+	assert.Equal(t, nil, err)
 	assert.NotEqual(t, false, resp)
 }
