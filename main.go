ea6e0f10f22f08e6f94fb8a6ce4297aaad37c6cd
Jani Anttonen <jantto@utu.fi>
2019-08-13 10:06:55 +0300

    make main.go clearer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/eqlabs/sprawl/api"
 	"github.com/eqlabs/sprawl/db"
+	"github.com/eqlabs/sprawl/p2p"
 )
 
 func main() {
@@ -12,6 +13,10 @@ func main() {
 	storage.Run()
 	defer storage.Close()
 
-	// p2pImpl := &p2p.P2p{}
+	// Run the P2P process
+	p2pInstance := p2p.P2p{}
+	p2pInstance.Run()
+
+	// Run the gRPC API
 	api.Run(storage, 1337)
 }
