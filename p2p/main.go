4f8ae6b71015a0b6c7f4bd939bb87e5b0df31226
Kristian Wahlroos <kjhwah@utu.fi>
2019-08-23 00:54:31 +0300

    Add pubsub and format project a bit

--- p2p/main.go
+++ p2p/main.go
@@ -1,165 +1,75 @@
 package p2p
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"sync"
-	"time"
 
 	libp2p "github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
-	network "github.com/libp2p/go-libp2p-core/network"
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	configt "github.com/libp2p/go-libp2p/config"
 	routing "github.com/libp2p/go-libp2p-core/routing"
-	protocol "github.com/libp2p/go-libp2p-core/protocol"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	configt "github.com/libp2p/go-libp2p/config"
 	multiaddr "github.com/multiformats/go-multiaddr"
-	// discovery "github.com/libp2p/go-libp2p/p2p/discovery"
 )
 
-const pubsubTopic = "/"
-const pubsubTopics = "/ssss"
+// A new type we need for writing a custom flag parser
+type addrList []multiaddr.Multiaddr
+const BaseTopic = "/sprawl/"
 
-// func (p2p *P2p)addPeers(ps *pubsub.PubSub, config Config){
-// 	for peer := range p2p.peerChan {
-// 		if peer.ID == p2p.host.ID() {
-// 			continue
-// 		}
-// 		ps.AddPeer(peer.ID, protocol.ID(config.ProtocolID))
-// 		fmt.Println("SOmething happenedd!!!!!!!!!")
-// 	}
-// }
+func createTopicString(topic string) string {
+	return BaseTopic + topic
+}
 
-func publishaa(ps *pubsub.PubSub, ctx context.Context, hos host.Host){
-	for {
-		err := ps.Publish(pubsubTopic, []byte("MÄMMI"))
-		fmt.Println("jouuuuuuussbost ")
-		if err != nil {
-			panic(err)
-		}
-		time.Sleep(5 * time.Second)
-		fmt.Println("jouuuuuuussbostsadsadg %s", ps.ListPeers(pubsubTopic))
-		fmt.Println("jouuuuuuussdg %s", hos.Addrs())
-		fmt.Println("jouuuusstsadsadg %s", hos.Peerstore())
+func PublishMessage(ps *pubsub.PubSub, topic string, input []byte) {
+	err := ps.Publish(createTopicString(topic), input)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func (p2p *P2p) pubsub(ctx context.Context, host host.Host) {
-	fmt.Println("jouuuuuusdsdsdu")
-	ps, err := pubsub.NewGossipSub(ctx, host)
+func (p2p *P2p) initPubSub(ctx context.Context, host host.Host) {
+	var err error
+	p2p.ps, err = pubsub.NewGossipSub(ctx, host)
 	if err != nil {
 		panic(err)
 	}
-	sub, err := ps.Subscribe(pubsubTopic)
+}
+
+func Subscribe(ps *pubsub.PubSub, ctx context.Context, topic string) {
+	sub, err := ps.Subscribe(createTopicString(topic))
 	if err != nil {
 		panic(err)
 	}
-	go publishaa(ps, ctx, host)
-	// go p2p.addPeers(ps, p2p.config)
-	fmt.Println("Mammi")
-	for {
-		msg, _ := sub.Next(ctx)
-		fmt.Println("TULEE VARMASTI %s", msg)
-	}
+	go func(ctx context.Context) {
+		for {
+			msg, err := sub.Next(ctx)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Message: %s", msg)
+		}
+	}(ctx)
 }
 
 type P2p struct {
-	config           Config
+	ps               *pubsub.PubSub
 	ctx              context.Context
 	host             host.Host
 	kademliaDHT      *dht.IpfsDHT
 	routingDiscovery *discovery.RoutingDiscovery
 	peerChan         <-chan peer.AddrInfo
+	bootstrapPeers   addrList
 }
 
-func handleStream(stream network.Stream) {
-	// Create a buffer stream for non blocking read and write.
-	reader := bufio.NewReader(stream)
-
-	go readData(reader, p2p.host)
-	writer := bufio.NewWriter(stream)
-	writeData(writer, []byte("ALa sano sitaasddsa!\n"))
-
-	// 'stream' will stay open until you close it (or the other side closes it).
-}
-
-func readData(reader *bufio.Reader, hos host.Host) {
-	for {
-		fmt.Println("mitt'r")
-		bytes, err := reader.ReadBytes(byte('\n'))
-		if err != nil {
-			fmt.Println("Error reading from buffer")
-			panic(err)
-		}
-		if bytes == nil {
-			fmt.Println("mitt'r")
-			return
-		}
-		if bytes[0] != byte('\n') {
-			// Green console colour: 	\x1b[32m
-			// Reset console colour: 	\x1b[0m
-			fmt.Println("\x1b[32m%s\x1b[0m> ", bytes)
-			fmt.Println("\x1b[32m%s\x1b[0m> ", hos.Peerstore)
-			fmt.Println("\x1b[32mb%s\x1b[0m> ", hos.Addrs())
-		}
-	}
-}
-
-func writeData(writer *bufio.Writer, input []byte) {
-	fmt.Println("Testi132")
-	_, err := writer.Write(input)
-	if err != nil {
-		fmt.Println("Error writing to buffer")
-		panic(err)
-	}
-
-	err = writer.Flush()
-	if err != nil {
-		fmt.Println("Error flushing buffer")
-		panic(err)
-	}
-}
-
-func (p2p *P2p) createConfig() {
-	var err error
-	p2p.config, err = ParseFlags()
-	fmt.Println("%s", p2p.config.ListenAddresses)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (p2p *P2p) createContext() {
+func (p2p *P2p) initContext() {
 	p2p.ctx = context.Background()
 }
 
-func (p2p *P2p) createHost(ctx context.Context, config Config) {
-	var err error
-	p2p.host, err = libp2p.New(ctx,
-		libp2p.ListenAddrs([]multiaddr.Multiaddr(config.ListenAddresses)...),
-	)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (p2p *P2p) createKademliaDHT(ctx context.Context, host host.Host) {
-	// Start a DHT, for use in peer discovery. We can't just make a new DHT
-	// client because we want each peer to maintain its own local copy of the
-	// DHT, so that the bootstrapping node of the DHT can go down without
-	// inhibiting future peer discovery.
-	var err error
-	p2p.kademliaDHT, err = dht.New(ctx, host)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (p2p *P2p) bootstrapDHT(ctx context.Context, kademliaDHT *dht.IpfsDHT) {
+func bootstrapDHT(ctx context.Context, kademliaDHT *dht.IpfsDHT) {
 	// Bootstrap the DHT. In the default configuration, this spawns a Background
 	// thread that will refresh the peer table every five minutes.
 	var err error
@@ -168,11 +78,17 @@ func (p2p *P2p) bootstrapDHT(ctx context.Context, kademliaDHT *dht.IpfsDHT) {
 	}
 }
 
-func (p2p *P2p) getPeerAddresses(ctx context.Context, config Config, host host.Host) {
-	// Let's connect to the bootstrap nodes first. They will tell us about the
-	// other nodes in the network.
+func (p2p *P2p) initBootstrapPeers(bootstrapPeers addrList) {
+	p2p.bootstrapPeers = bootstrapPeers
+}
+
+func (p2p *P2p) addDefaultBootstrapPeers() {
+	p2p.initBootstrapPeers(dht.DefaultBootstrapPeers)
+}
+
+func connectToPeers(ctx context.Context, host host.Host, bootstrapPeers addrList) {
 	var wg sync.WaitGroup
-	for _, peerAddr := range config.BootstrapPeers {
+	for _, peerAddr := range bootstrapPeers {
 		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
 		wg.Add(1)
 		go func() {
@@ -180,7 +96,7 @@ func (p2p *P2p) getPeerAddresses(ctx context.Context, config Config, host host.H
 			if err := host.Connect(ctx, *peerinfo); err != nil {
 				fmt.Println(err)
 			} else {
-				fmt.Println("Kekkonen + %s", peerinfo)
+				fmt.Println("Connected to : %s", peerinfo)
 			}
 		}()
 	}
@@ -191,60 +107,19 @@ func (p2p *P2p) createRoutingDiscovery(kademliaDHT *dht.IpfsDHT) {
 	p2p.routingDiscovery = discovery.NewRoutingDiscovery(kademliaDHT)
 }
 
-func (p2p *P2p) advertise(ctx context.Context, config Config, routingDiscovery *discovery.RoutingDiscovery) {
-	discovery.Advertise(ctx, routingDiscovery, config.RendezvousString)
+func advertise(ctx context.Context, routingDiscovery *discovery.RoutingDiscovery) {
+	discovery.Advertise(ctx, routingDiscovery, BaseTopic)
 }
 
-func (p2p *P2p) findPeers(ctx context.Context, config Config, routingDiscovery *discovery.RoutingDiscovery) {
+func (p2p *P2p) findPeers(ctx context.Context, routingDiscovery *discovery.RoutingDiscovery) {
 	var err error
-	p2p.peerChan, err = routingDiscovery.FindPeers(ctx, config.RendezvousString)
+	p2p.peerChan, err = routingDiscovery.FindPeers(ctx, BaseTopic)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func (p2p *P2p) SendToPeers(input []byte) {
-	p2p.sendToPeers(p2p.ctx, p2p.config, p2p.host, p2p.peerChan, input)
-}
-
-func (p2p *P2p) sendToPeers(ctx context.Context, config Config, host host.Host, peerChan <-chan peer.AddrInfo, input []byte) {
-	for peer := range peerChan {
-		if peer.ID == host.ID() {
-			continue
-		}
-		stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
-
-		if err != nil {
-			continue
-		} else {
-			writer := bufio.NewWriter(stream)
-			writeData(writer, input)
-		}
-		}
-	}
-
-
-func (p2p *P2p) listenPeers(ctx context.Context, config Config, host host.Host, peerChan <-chan peer.AddrInfo) {
-
-	for peer := range peerChan {
-		if peer.ID == host.ID() {
-			continue
-		}
-		stream, err := host.NewStream(ctx, peer.ID, protocol.ID(config.ProtocolID))
-
-		if err != nil {
-			continue
-		} else {
-			reader := bufio.NewReader(stream)
-			go readData(reader, host)
-			writer := bufio.NewWriter(stream)
-			writeData(writer, []byte("ALa sano sita!\n"))
-		}
-	}
-}
-
-//KAUNIS MIELI MOODI
-func (p2p *P2p) tinamenkka() configt.Option {
+func (p2p *P2p) initDHT() configt.Option {
 	NewDHT := func(h host.Host) (routing.PeerRouting, error) {
 		var err error
 		p2p.kademliaDHT, err = dht.New(p2p.ctx, h)
@@ -254,10 +129,13 @@ func (p2p *P2p) tinamenkka() configt.Option {
 
 }
 
-func (p2p *P2p) tinamämmi(routing configt.Option) {
+func (p2p *P2p) initHost(routing configt.Option) {
 	var err error
 	p2p.host, err = libp2p.New(p2p.ctx,
-		libp2p.ListenAddrs([]multiaddr.Multiaddr(p2p.config.ListenAddresses)...), routing,
+		routing,
+		libp2p.EnableRelay(),
+		libp2p.EnableAutoRelay(),
+		libp2p.NATPortMap(),
 	)
 	if err != nil {
 		panic(err)
@@ -266,32 +144,14 @@ func (p2p *P2p) tinamämmi(routing configt.Option) {
 
 // Run runs the p2p network
 func (p2p *P2p) Run() {
-	// Set a function as stream handler. This function is called when a peer
-	// initiates a connection and starts a stream with this peer.
-	p2p.createConfig()
-	fmt.Println("1Loppu")
-	p2p.createContext()
-	fmt.Println("2Loppu")
-	// p2p.createHost(p2p.ctx, p2p.config)
-	fmt.Println("3Loppu")
-	p2p.tinamämmi(p2p.tinamenkka())
-	// p2p.createKademliaDHT(p2p.ctx, p2p.host)
-	fmt.Println("4Loppu")
-	p2p.host.SetStreamHandler(protocol.ID(p2p.config.ProtocolID), p2p.handleStream)
-	fmt.Println("6Loppu")
-	p2p.getPeerAddresses(p2p.ctx, p2p.config, p2p.host)
-	fmt.Println("7Loppu")
+	p2p.initContext()
+	p2p.initHost(p2p.initDHT())
+	p2p.addDefaultBootstrapPeers()
+	connectToPeers(p2p.ctx, p2p.host, p2p.bootstrapPeers)
 	p2p.createRoutingDiscovery(p2p.kademliaDHT)
-	fmt.Println("8Loppu")
-	p2p.advertise(p2p.ctx, p2p.config, p2p.routingDiscovery)
-	fmt.Println("9Loppu")
-	p2p.findPeers(p2p.ctx, p2p.config, p2p.routingDiscovery)
-	fmt.Println("10Loppu")
-	fmt.Println("5Loppu")
-	p2p.bootstrapDHT(p2p.ctx, p2p.kademliaDHT)
-	p2p.pubsub(p2p.ctx, p2p.host)
-	// fmt.Println("11Loppu")
-	// p2p.listenPeers(p2p.ctx, p2p.config, p2p.host, p2p.peerChan)
-	fmt.Println("12Loppu")
+	advertise(p2p.ctx, p2p.routingDiscovery)
+	p2p.findPeers(p2p.ctx, p2p.routingDiscovery)
+	p2p.initPubSub(p2p.ctx, p2p.host)
+	bootstrapDHT(p2p.ctx, p2p.kademliaDHT)
 	select {}
 }
