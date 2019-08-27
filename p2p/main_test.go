9f3f9e615394cc6e24bed47b01b5cfb578f780e0
KristianWahlroos <kjhwah@utu.fi>
2019-08-27 16:31:38 +0300

    Formatting

--- p2p/main_test.go
+++ p2p/main_test.go
@@ -8,8 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const test_topic string = "test_topic"
+
+var test_data []byte = []byte("test_data")
+
 func TestCreateTopicString(t *testing.T) {
-	assert.Equal(t, createTopicString("test_topic"), "/sprawl/test_topic")
+	assert.Equal(t, createTopicString(test_topic), baseTopic+test_topic)
 }
 
 func TestInitContext(t *testing.T) {
@@ -21,12 +25,12 @@ func TestInitContext(t *testing.T) {
 func TestInput(t *testing.T) {
 	p2pInstance := NewP2p()
 	go func() {
-		p2pInstance.Input([]byte("test_data"), "test_topic")
+		p2pInstance.Input(test_data, test_topic)
 	}()
 	select {
 	case message := <-p2pInstance.input:
-		assert.Equal(t, message.topic, "test_topic")
-		assert.Equal(t, message.data, []byte("test_data"))
+		assert.Equal(t, message.topic, test_topic)
+		assert.Equal(t, message.data, test_data)
 	}
 }
 
@@ -35,14 +39,14 @@ func TestPublish(t *testing.T) {
 	p2pInstance.initContext()
 	p2pInstance.host, _ = libp2p.New(p2pInstance.ctx)
 	p2pInstance.initPubSub()
-	sub, _ := p2pInstance.ps.Subscribe(createTopicString("test_topic"))
+	sub, _ := p2pInstance.ps.Subscribe(createTopicString(test_topic))
 	go func() {
-		p2pInstance.Input([]byte("test_data"), "test_topic")
+		p2pInstance.Input(test_data, test_topic)
 	}()
 	select {
 	case message := <-p2pInstance.input:
 		p2pInstance.handleInput(message)
 		msg, _ := sub.Next(p2pInstance.ctx)
-		assert.Equal(t, msg.Data, []byte("test_data"))
+		assert.Equal(t, msg.Data, test_data)
 	}
 }
