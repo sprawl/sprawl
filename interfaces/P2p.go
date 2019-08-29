7449f31df9b915775186e2e1ccd45a2a66b20f68
KristianWahlroos <kjhwah@utu.fi>
2019-08-29 14:49:35 +0300

    Fix errors

--- interfaces/P2p.go
+++ interfaces/P2p.go
@@ -7,7 +7,7 @@ import (
 type P2p interface {
 	RegisterOrderService(orders OrderService)
 	RegisterChannelService(channels ChannelService)
-	Input(data []byte, channel *pb.Channel)
-	Subscribe(channel *pb.Channel)
+	Input(data []byte, channel pb.Channel)
+	Subscribe(channel pb.Channel)
 	Run()
 }
