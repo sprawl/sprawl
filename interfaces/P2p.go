aa5cc8ed4f23bae9eb80d3658661aa6c012f05a2
KristianWahlroos <kjhwah@utu.fi>
2019-08-29 14:32:49 +0300

    Update p2p interface

--- interfaces/P2p.go
+++ interfaces/P2p.go
@@ -1,9 +1,13 @@
 package interfaces
 
+import (
+	"github.com/eqlabs/sprawl/pb"
+)
+
 type P2p interface {
 	RegisterOrderService(orders OrderService)
 	RegisterChannelService(channels ChannelService)
-	Input(order []byte, channel string)
-	Subscribe(channel string)
+	Input(data []byte, channel *pb.Channel)
+	Subscribe(channel *pb.Channel)
 	Run()
 }
