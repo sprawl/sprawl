757b022cc3fe4a430ac6ccb55d14d1554ffecd45
Jani Anttonen <jantto@utu.fi>
2019-08-29 13:53:34 +0300

    add GetAllOrders endpoint to gRPC

--- service/order_test.go
+++ service/order_test.go
@@ -30,6 +30,37 @@ var lis *bufconn.Listener
 var conn *grpc.ClientConn
 var err error
 var ctx context.Context
+var storage *db.Storage = &db.Storage{}
+var p2pInstance *p2p.P2p = p2p.NewP2p()
+var testConfig *config.Config = &config.Config{}
+var s *grpc.Server
+var orderClient pb.OrderHandlerClient
+
+func init() {
+	testConfig.ReadConfig(testConfigPath)
+	storage.SetDbPath(testConfig.GetString(dbPathVar))
+}
+
+func createNewServerInstance() {
+	p2pInstance.Run()
+	storage.Run()
+
+	ctx = context.Background()
+	lis = bufconn.Listen(bufSize)
+
+	conn, err = grpc.DialContext(ctx, dialContext, grpc.WithDialer(BufDialer), grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	s = grpc.NewServer()
+
+	orderClient = pb.NewOrderHandlerClient(conn)
+}
+
+func removeAllOrders() {
+	storage.DeleteAllWithPrefix(string(interfaces.OrderPrefix))
+}
 
 func BufDialer(string, time.Duration) (net.Conn, error) {
 	return lis.Dial()
@@ -41,31 +72,11 @@ func TestOrderStorageKeyPrefixer(t *testing.T) {
 }
 
 func TestOrderCreation(t *testing.T) {
-	var storage *db.Storage = &db.Storage{}
-	var p2pInstance *p2p.P2p = p2p.NewP2p()
-	p2pInstance.Run()
+	createNewServerInstance()
 	defer p2pInstance.Close()
-
-	// Load config
-	config := &config.Config{}
-	config.ReadConfig(testConfigPath)
-
-	// Initialize storage
-	storage.SetDbPath(config.GetString(dbPathVar))
-	storage.Run()
 	defer storage.Close()
-
-	ctx = context.Background()
-	lis = bufconn.Listen(bufSize)
-
-	conn, err = grpc.DialContext(ctx, dialContext, grpc.WithDialer(BufDialer), grpc.WithInsecure())
-	if err != nil {
-		panic(err)
-	}
 	defer conn.Close()
-
-	// Create gRPC server
-	s := grpc.NewServer()
+	removeAllOrders()
 
 	testOrder := pb.CreateRequest{Asset: []byte(asset1), CounterAsset: []byte(asset2), Amount: testAmount, Price: testPrice}
 
@@ -86,8 +97,6 @@ func TestOrderCreation(t *testing.T) {
 		defer s.Stop()
 	}()
 
-	var orderClient pb.OrderHandlerClient = pb.NewOrderHandlerClient(conn)
-
 	resp, err := orderClient.Create(ctx, &testOrder)
 	assert.Equal(t, nil, err)
 	t.Log("Created Order: ", resp)
@@ -98,4 +107,44 @@ func TestOrderCreation(t *testing.T) {
 	resp2, err := orderClient.Delete(ctx, &pb.OrderSpecificRequest{Id: lastOrder.GetId()})
 	assert.Equal(t, nil, err)
 	assert.NotEqual(t, false, resp2)
+}
+
+func TestOrderGetAll(t *testing.T) {
+	createNewServerInstance()
+	defer p2pInstance.Close()
+	defer storage.Close()
+	defer conn.Close()
+	removeAllOrders()
+
+	testOrder := pb.CreateRequest{Asset: []byte(asset1), CounterAsset: []byte(asset2), Amount: testAmount, Price: testPrice}
+
+	// Create an OrderService
+	var orderService interfaces.OrderService = &OrderService{}
+	// Register services
+	orderService.RegisterStorage(storage)
+	orderService.RegisterP2p(p2pInstance)
+	// Register order endpoints with the gRPC server
+	pb.RegisterOrderHandlerServer(s, orderService)
+
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+		defer s.Stop()
+	}()
+
+	const testIterations = int(4)
+	for i := 0; i < testIterations; i++ {
+		_, err := orderClient.Create(ctx, &testOrder)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	resp, err := orderClient.GetAll(ctx, &pb.Empty{})
+	if err != nil {
+		panic(err)
+	}
+	orders := resp.GetOrders()
+	assert.Equal(t, len(orders), testIterations)
 }
