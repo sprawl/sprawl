c4a4e8728b2bf255828fa7e21e8455a08875a41b
Jani Anttonen <jantto@utu.fi>
2019-09-10 13:00:04 +0300

    reduce the amount of logs

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -104,7 +104,7 @@ func (p2p *P2p) handleInput(message *pb.WireMessage) {
 
 // Send queues a message for sending to other peers
 func (p2p *P2p) Send(message *pb.WireMessage) {
-	log.Infof("Sending order %s to channel %s", message.GetData(), message.GetChannelID())
+	log.Debugf("Sending order %s to channel %s", message.GetData(), message.GetChannelID())
 
 	go func(ctx context.Context) {
 		p2p.input <- *message
