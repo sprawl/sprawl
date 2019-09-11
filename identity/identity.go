77de37b92fd5e70116b82bf1ea2f6d1dd3550d21
KristianWahlroos <kjhwah@utu.fi>
2019-09-11 12:33:51 +0300

    Add store and get keypair methods to identity

--- identity/identity.go
+++ identity/identity.go
@@ -1,14 +1,70 @@
 package identity
 
 import (
-	"io"
+	"github.com/eqlabs/sprawl/interfaces"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/prometheus/common/log"
+	"io"
 )
+
+const privateKeyDbKey = "private_key"
+const publicKeyDbKey = "public_key"
 
 func GenerateKeyPair(reader io.Reader) (crypto.PrivKey, crypto.PubKey) {
 	var err error
 	privateKey, publicKey, err := crypto.GenerateEd25519Key(reader)
-	log.Error(err)
+	if err != nil {
+		log.Error(err)
+	}
+	return privateKey, publicKey
+}
+
+func StoreKeyPair(storage interfaces.Storage, privateKey crypto.PrivKey, publicKey crypto.PubKey) {
+	privateKeyBytes, err := crypto.MarshalPrivateKey(privateKey)
+	if err != nil {
+		log.Error(err)
+	}
+
+	publicKeyBytes, err := crypto.MarshalPublicKey(publicKey)
+	if err != nil {
+		log.Error(err)
+	}
+
+	err = storage.Put([]byte(privateKeyDbKey), privateKeyBytes)
+	if err != nil {
+		log.Error(err)
+	}
+
+	err = storage.Put([]byte(publicKeyDbKey), publicKeyBytes)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func GetKeyPair(storage interfaces.Storage) (crypto.PrivKey, crypto.PubKey) {
+	var err error
+	privateKeyBytes, err := storage.Get([]byte(privateKeyDbKey))
+	if err != nil {
+		log.Error(err)
+		return nil, nil
+	}
+	publicKeyBytes, err := storage.Get([]byte(publicKeyDbKey))
+	if err != nil {
+		log.Error(err)
+		return nil, nil
+	}
+
+	privateKey, err := crypto.UnmarshalEd25519PrivateKey(privateKeyBytes)
+	if err != nil {
+		log.Error(err)
+		return nil, nil
+	}
+
+	publicKey, err := crypto.UnmarshalEd25519PublicKey(publicKeyBytes)
+	if err != nil {
+		log.Error(err)
+		return nil, nil
+	}
+
 	return privateKey, publicKey
 }
