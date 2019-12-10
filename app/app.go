913979be34d456ce376b2cb45bde9bc0cba3d5d9
Jani Anttonen <jantto@utu.fi>
2019-12-11 00:24:59 +0200

    remove blocking order

--- app/app.go
+++ app/app.go
@@ -101,8 +101,6 @@ func (app *App) InitServices(config interfaces.Config, Logger interfaces.Logger)
 
 // Run is a separated main-function to ease testing
 func (app *App) Run() {
-	// Run the gRPC API
-
 	defer app.Server.Close()
 	defer app.Storage.Close()
 	defer app.P2p.Close()
@@ -113,5 +111,7 @@ func (app *App) Run() {
 		}
 		go app.debugPinger()
 	}
+
+	// Run the gRPC API
 	app.Server.Run(app.config.GetUint("rpc.port"))
 }
