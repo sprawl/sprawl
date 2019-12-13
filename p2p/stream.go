234e87a6823e12fbe3514d30be6b0938a11fa522
Jani Anttonen <jantto@utu.fi>
2019-12-13 15:14:21 +0200

    add private streams

---
+++ p2p/stream.go
@@ -0,0 +1,80 @@
+package p2p
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/libp2p/go-libp2p-core/network"
+	peer "github.com/libp2p/go-libp2p-peer"
+)
+
+func handleStream(stream network.Stream) {
+	fmt.Println("Got a new stream!")
+
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+	go readData(rw)
+	go writeData(rw)
+
+	// 'stream' will stay open until you close it (or the other side closes it).
+}
+
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, err := rw.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from buffer")
+			panic(err)
+		}
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from stdin")
+			panic(err)
+		}
+
+		_, err = rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		if err != nil {
+			fmt.Println("Error writing to buffer")
+			panic(err)
+		}
+		err = rw.Flush()
+		if err != nil {
+			fmt.Println("Error flushing buffer")
+			panic(err)
+		}
+	}
+}
+
+func (p2p *P2p) openPrivateConnection(peerIDString string) error {
+	p2p.host.SetStreamHandler(networkID, handleStream)
+	peerID, err := peer.IDFromString(peerIDString)
+	stream, err := p2p.host.NewStream(p2p.ctx, peerID, networkID)
+	if err != nil {
+		fmt.Println("Stream open failed", err)
+	} else {
+		rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+		go writeData(rw)
+		go readData(rw)
+		fmt.Println("Connected to:", peerID)
+	}
+	return err
+}
