e89f2052300c949eb9a58505f64b9eef2c84c13b
Jani Anttonen <jantto@utu.fi>
2020-01-21 13:37:49 +0200

    fix build

--- interfaces/NodeService.go
+++ interfaces/NodeService.go
@@ -9,5 +9,5 @@ import (
 type NodeService interface {
 	RegisterP2p(p2p P2p)
 	GetAllPeers(ctx context.Context, in *pb.Empty) (*pb.PeerListResponse, error)
-	BlacklistPeer(ctx context.Context, in *pb.Peer) (*pb.GenericResponse, error)
+	BlacklistPeer(ctx context.Context, in *pb.Peer) (*pb.Empty, error)
 }
--- service/node.go
+++ service/node.go
@@ -7,24 +7,25 @@ import (
 	"github.com/sprawl/sprawl/pb"
 )
 
-// ChannelService implements the ChannelHandlerServer service.proto
+// NodeService is a gRPC service for p2p operations.
 type NodeService struct {
 	P2p interfaces.P2p
 }
 
+// RegisterP2p registers a p2p interface with NodeService
 func (s *NodeService) RegisterP2p(p2p interfaces.P2p) {
 	s.P2p = p2p
 }
 
+// GetAllPeers fetches all connected peers from NodeService.P2p
 func (s *NodeService) GetAllPeers(ctx context.Context, in *pb.Empty) (*pb.PeerListResponse, error) {
 	data := s.P2p.GetAllPeers()
 	peerList := &pb.PeerListResponse{PeerIds: data}
 	return peerList, nil
 }
 
-func (s *NodeService) BlacklistPeer(ctx context.Context, in *pb.Peer) (*pb.GenericResponse, error) {
+// BlacklistPeer blacklists a peer from connecting to this node
+func (s *NodeService) BlacklistPeer(ctx context.Context, in *pb.Peer) (*pb.Empty, error) {
 	s.P2p.BlacklistPeer(in)
-	return &pb.GenericResponse{
-		Error: nil,
-	}, nil
+	return &pb.Empty{}, nil
 }
