0101007c647a974d0e90bc33e9bc31be9d4e5c8b
kristianwahlroos <kristian.whl@gmail.com>
2020-01-22 15:31:26 +0200

    Update orderservice interface

--- interfaces/OrderService.go
+++ interfaces/OrderService.go
@@ -10,6 +10,8 @@ import (
 type OrderService interface {
 	RegisterStorage(db Storage)
 	RegisterP2p(p2p P2p)
+	RegisterWebsocket(websocket WebsocketService)
+	StartWebsocket()
 	Create(ctx context.Context, in *pb.CreateRequest) (*pb.CreateResponse, error)
 	Receive(in []byte) error
 	Delete(ctx context.Context, in *pb.OrderSpecificRequest) (*pb.GenericResponse, error)
