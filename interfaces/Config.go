f2450056edbf6cd9bcbbe6a0641d5e35b62f75fc
kristianwahlroos <kristian.whl@gmail.com>
2020-02-10 15:03:00 +0200

    Update config interface

--- interfaces/Config.go
+++ interfaces/Config.go
@@ -2,6 +2,10 @@ package interfaces
 
 // Config is an interface to viper
 type Config interface {
+	AddString(key string)
+	AddBoolean(key string)
+	AddStringE(key string) error
+	AddBooleanE(key string) error
 	ReadConfig(configPath string)
 	GetDatabasePath() string
 	GetExternalIP() string
